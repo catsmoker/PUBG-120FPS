0e9111d140a7ad61c23d12e295b28e5d6e2892bd
catsmoker <119059457+catsmoker@users.noreply.github.com>
2024-08-07 17:33:25 +0100

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
+# Print installation status messages
 ui_print "---------------------------------"
 ui_print "          FPS Unlocker           "
-ui_print "       made by catsmoker         "
+ui_print "       Made by catsmoker         "
 ui_print "---------------------------------"
-ui_print "Successfully changed device model"
+ui_print "Successfully changed device model."
 ui_print "          Happy gaming!          "
 ui_print "---------------------------------"
